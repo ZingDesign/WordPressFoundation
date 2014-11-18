46fc7f5619d294ba10931dacf2de32cb82bc213d
Sam <sam@zingdesign.co.nz>
2014-11-19 11:51:02 +1300

    Fix social sharing link issues

--- content-social.php
+++ content-social.php
@@ -9,7 +9,7 @@
 $post_link       = urlencode( get_permalink() );
 $post_thumb_url  = has_post_thumbnail() ? urlencode( wp_get_attachment_url( get_post_thumbnail_id() ) ) : '';
 $post_title      = urlencode( get_the_title() );
-$post_summary    = urlencode( get_the_excerpt() );
+$post_summary    = urlencode( strip_tags( get_the_excerpt() ) );
 $twitter_account = urlencode( preg_replace( "/https?\:\/\/twitter\.com\//", "", get_option( 'twitter-url' ) ) );
 $tweet_text      = sprintf( __( 'Check out %1$s on %2$s', 'zingdesign' ), $post_title, get_bloginfo( 'name' ) );
 
@@ -29,15 +29,15 @@ $share_google  = 'https://plus.google.com/share?url=' . $post_link;
 
 	<div class="row">
 		<div class="medium-4 columns">
-			<a href="<?php echo esc_url( $share_facebook ); ?>" class="button facebook" target="_blank"
+			<a href="<?php echo ( $share_facebook ); ?>" class="button facebook" target="_blank"
 			   title="Share on Facebook"><i class="fa fa-facebook fa-right-margin"></i> Facebook</a>
 		</div>
 		<div class="medium-4 columns">
-			<a href="<?php echo esc_url( $share_twitter ); ?>" class="button twitter" target="_blank"
+			<a href="<?php echo ( $share_twitter ); ?>" class="button twitter" target="_blank"
 			   title="Tweet about this post"><i class="fa fa-twitter fa-right-margin"></i> Twitter</a>
 		</div>
 		<div class="medium-4 columns">
-			<a href="<?php echo esc_url( $share_google ); ?>" class="button google-plus" target="_blank"
+			<a href="<?php echo ( $share_google ); ?>" class="button google-plus" target="_blank"
 			   title="Share on Google+"><i class="fa fa-google-plus fa-right-margin"></i> Google+</a>
 		</div>
 	</div>
